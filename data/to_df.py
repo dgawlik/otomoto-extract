6e9ad2feaade631c24f14fbbd3ae4f8ae27869f6
Dominik Gawlik <dominik.gawlik1@gmail.com>
2026-01-23 10:50:09 +0100

    integers

--- data/to_df.py
+++ data/to_df.py
@@ -18,5 +18,34 @@ for f in files:
 
 df_stacked = pd.concat(frames, axis=0)
 
-rel = duckdb.query("SELECT * FROM df_stacked")
+import re
+
+def power_to_int(pow):
+    try:
+        clean_pow = re.sub('\\s+KM', '', pow)
+        return int(clean_pow)
+    except:
+        return -1
+    
+def mileage_to_int(mileage):
+    try:
+        clean_mileage = re.sub('\\s+km', '', mileage)
+        clean_mileage = re.sub('\\s+', '', clean_mileage)
+        return int(clean_mileage)
+    except:
+        return -1
+    
+def price_to_int(price):
+    try:
+        clean_price = re.sub('\\s+PLN', '', price)
+        clean_price = re.sub('\\s+', '', clean_price)
+        return int(clean_price)
+    except:
+        return -1
+
+df_stacked['power'] = df_stacked['power'].apply(power_to_int)
+df_stacked['mileage'] = df_stacked['mileage'].apply(mileage_to_int)
+df_stacked['price'] = df_stacked['price'].apply(price_to_int)
+
+rel = duckdb.query("SELECT * FROM df_stacked WHERE price < 15000")
 rel.show(max_width=200, max_rows=1000)
